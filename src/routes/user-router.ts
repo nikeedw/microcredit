4aefaedf8f7d6871285e3e459b9bde7c772c598e
Edward Nikolaev <nikolaev.eduard.vasilievich@gmail.com>
2024-09-28 21:50:13 +0300

    fix: fixed req type to post

--- src/routes/user-router.ts
+++ src/routes/user-router.ts
@@ -7,6 +7,6 @@ const router = Router()
 router.post('/register', UserHandler.register)
 router.post('/login', UserHandler.login)
 router.get('/current', checkJwt, UserHandler.current)
-router.get('/send-email', UserHandler.sendEmail)
+router.post('/send-email', checkJwt, UserHandler.sendEmail)
 
 export default router
